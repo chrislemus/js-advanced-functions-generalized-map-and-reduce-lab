10362ede064feaa9c975b1e30daf9fd88b8bf024
chrislemus <icrislemus@gmail.com>
2021-03-17 10:12:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+
+
+const map = (arr, func) => {
+  return arr.map(item => func(item) )
+}
+
+const reduce = (arr, func, startVal = false) => {
+  return startVal ? arr.reduce(func, startVal) : arr.reduce(func)
+}
